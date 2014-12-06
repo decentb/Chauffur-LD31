05c7824495db1d3a44bd0c56133ef0af32ed6534
Blake Bouchard <thatsblake@gmail.com>
2014-12-05 20:19:30 -0800

    Road Stripes are Moving

---
+++ Assets/Scripts/Background/GrowStripe.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrowStripe : MonoBehaviour {
+
+    public float scaleIncrease = 0.01f;
+    public float timeBetweenScale = 1.0f;
+
+	// Use this for initialization
+	void Start()
+    {
+        StartCoroutine(ScaleIncrease(scaleIncrease, timeBetweenScale));
+	}
+
+    IEnumerator ScaleIncrease(float sizeIncrease, float timeBetweenScale)
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(timeBetweenScale);
+            transform.localScale *= scaleIncrease;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update()
+    {
+	    
+	}
+}
---
+++ Assets/Scripts/Spawners/SpawnRoadStripes.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnRoadStripes : MonoBehaviour {
+
+    public float timeBetweenStripes = 1.0f;
+    public float stripeSpeed = 1.0f;
+    public Transform roadStripePrefab;
+
+	// Use this for initialization
+	void Start()
+    {
+        StartCoroutine(SpawnStripes());
+	}
+
+    IEnumerator SpawnStripes()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(timeBetweenStripes);
+            Transform roadStripe = (Transform)Instantiate(roadStripePrefab, this.transform.position, Quaternion.identity);
+            roadStripe.name = "Road Stripe";
+            roadStripe.rigidbody2D.velocity = new Vector2(0, stripeSpeed);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update()
+    {
+	
+	}
+}
