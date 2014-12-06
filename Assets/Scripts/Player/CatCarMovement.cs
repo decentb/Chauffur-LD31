a7e430d9c3e4d3b0c2f64d43131647edab051f15
Blake Bouchard <thatsblake@gmail.com>
2014-12-06 14:26:02 -0800

    Cat Paw and Car Door

--- Assets/Scripts/Player/CatCarMovement.cs
+++ Assets/Scripts/Player/CatCarMovement.cs
@@ -6,6 +6,8 @@ public class CatCarMovement : MonoBehaviour {
     float axisX = 0;
 
     public float horizontalSpeed = 1.0f;
+    public Transform catPaw;
+    public Transform carDoor;
 
     // Use this for initialization
     void Start()
@@ -21,5 +23,16 @@ public class CatCarMovement : MonoBehaviour {
 	void Update()
     {
         rigidbody2D.velocity = new Vector2(axisX * horizontalSpeed, 0);
+
+        if (transform.position.x < 0)
+        {
+            catPaw.gameObject.SetActive(true);
+            carDoor.gameObject.SetActive(false);
+        }
+        else
+        {
+            catPaw.gameObject.SetActive(false);
+            carDoor.gameObject.SetActive(true);
+        }
     }
 }
