691523f04537859bdfb4dde1963e7f93ba35695a
Blake Bouchard <thatsblake@gmail.com>
2014-12-05 20:24:32 -0800

    Nicer Stripe Movement

--- Assets/Scripts/Background/GrowStripe.cs
+++ Assets/Scripts/Background/GrowStripe.cs
@@ -3,21 +3,22 @@ using System.Collections;
 
 public class GrowStripe : MonoBehaviour {
 
-    public float scaleIncrease = 0.01f;
+    public float xScaleIncrease = 1.01f;
+    public float yScaleIncrease = 1.01f;
     public float timeBetweenScale = 1.0f;
 
 	// Use this for initialization
 	void Start()
     {
-        StartCoroutine(ScaleIncrease(scaleIncrease, timeBetweenScale));
+        StartCoroutine(ScaleIncrease());
 	}
 
-    IEnumerator ScaleIncrease(float sizeIncrease, float timeBetweenScale)
+    IEnumerator ScaleIncrease()
     {
-        while(true)
+        while (true)
         {
             yield return new WaitForSeconds(timeBetweenScale);
-            transform.localScale *= scaleIncrease;
+            transform.localScale = new Vector3(transform.localScale.x * xScaleIncrease, transform.localScale.y * yScaleIncrease, transform.localScale.z);
         }
     }
 	
